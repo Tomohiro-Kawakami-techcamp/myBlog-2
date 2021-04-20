0e933aeac16329cc542ace44c10beb820041d21f
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-04-20 11:16:04 +0900

    4/20時点

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,30 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(items_params)
+    if @item.save
+      redirect_to  items_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
