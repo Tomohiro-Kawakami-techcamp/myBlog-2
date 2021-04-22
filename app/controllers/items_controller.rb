2865425f6262af582294d31840ce77b3f69cfd97
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-04-22 10:33:17 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,15 +17,30 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      redirect_to  item_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+        redirect_to items_path
+    else
+        render :edit
+    end
   end
 
   private
